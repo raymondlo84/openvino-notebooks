e9000816775e96ca225ec4436d2f5a6450f60248
Kloosterman <helena.kloosterman@intel.com>
2021-03-19 15:09:36 +0100

    Added requirements.txt and install batch file

---
+++ 201-vision-monocular-depth-estimation/install_and_launch_monodepth.bat
@@ -0,0 +1,3 @@
+py -3.7 -m venv openvino_env_test
+openvino_env_test\Scripts\activate && python -m pip install --upgrade pip && python -m pip install -r requirements.txt && python -m ipykernel install --user --name openvino_env_test && jupyter lab
+
